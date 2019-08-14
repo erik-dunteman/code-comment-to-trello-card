6faf9e0784853596d10f09b85fefd24c09d08186
Erik Dunteman <erikdunteman@gmail.com>
2019-08-14 00:43:35 -0700

    First commit, with full code-comment-to-trello-card functionality built.

---
+++ todo_ex1.py
@@ -0,0 +1,9 @@
+### TODO: Touch the butt
+## TODO: Don't touch the butt
+todo = 5
+print(todo)
+TODO = todo
+print(TODO)
+list = ["this", "will", "be", 'irrelevant']
+### TODO: Make 55 gallons of hummus for the potluck.
+### TODO: Touch the butt again
---
+++ todo_ex2.py
@@ -0,0 +1,6 @@
+# This file is a dummy file
+# The system will flag lines starting with the trigger, in my case "### TODO:"
+
+### TODO: This will be a new card
+
+        ### TODO: This will also show up, even though it is indented!!
